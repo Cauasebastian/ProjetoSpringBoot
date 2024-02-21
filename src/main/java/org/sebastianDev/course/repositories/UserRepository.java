f6aafbfe39bd089f453b81b157e894226ae06982
Cauã Sebastian <cauasebastian2207@gmail.com>
2024-02-21 00:49:46 -0300

    service layer and userResource

--- src/main/java/org/sebastianDev/course/repositories/UserRepository.java
+++ src/main/java/org/sebastianDev/course/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package org.sebastianDev.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.sebastianDev.course.entities.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
